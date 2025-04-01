556bee35e7a25473f21f384250ce87860f43f77b
Camila Perez <camilap11517@gmail.com>
2025-04-01 12:00:36 -0300

    segundo commit: modificacion tp2_1_1.c

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #define N 20
 
 int main(){
@@ -7,7 +8,7 @@ int main(){
     for(i = 0; i<N; i++)
     {
         vt[i]=1+rand()%100;
-        printf("%f", vt[i]);
+        printf("\n%f", vt[i]);
     }
     return 0;
 }
